5c01b2d17d564b0ed8a235377c16503baa43d8e9
Nurlan <nurlan.grachyov@mail.ru>
2026-03-07 22:19:44 +0400

    fixed meeting validator

--- src/application/routers/router_meeting.py
+++ src/application/routers/router_meeting.py
@@ -60,7 +60,6 @@ async def create_meeting(
     Возвращает:
     - Схему MeetingGet с данными созданной встречи.
     """
-    print(meeting)
     try:
         user = await user_service.get_user_with_team_link(email=current_user.email)
     except LookupError:
